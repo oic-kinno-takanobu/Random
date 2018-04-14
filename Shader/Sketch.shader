fa7cc31e19350e938b534dd58f54cdf860b0270e
oic-kinno-takanobu <kinno1816@gmail.com>
2018-04-15 01:27:38 +0900

    シェーダー勉強記録

---
+++ Shader/Sketch.shader
@@ -0,0 +1,33 @@
+﻿Shader "ShaderSketch/Sketch"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+	CGINCLUDE
+	#include "UnityCG.cginc"
+
+	float4 frag(v2f_img i) : SV_Target
+	{
+		//return float4(i.uv.x,i.uv.y,0,1);
+		
+		float d = distance(float2(0.5,0.5),i.uv);
+		return d;
+
+
+	}
+	ENDCG
+
+	SubShader
+	{
+		
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			ENDCG
+		}
+	}
+}
---
+++ Source/Sketch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sketch : MonoBehaviour {
+
+    [SerializeField]
+    Material sketchMat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(null, destination, sketchMat);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
